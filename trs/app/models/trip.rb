ce643bc57e9fd9bb8d5522790e6759ad7de58e01
keyasuda <keyasuda@users.noreply.github.com>
2022-01-01 15:28:17 +0900

    root pathをtrips#indexに

--- trs/app/models/trip.rb
+++ trs/app/models/trip.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Trip < ApplicationRecord
-  has_many :days, dependent: :destroy
+  has_many :days,
+           -> { order('start_at') },
+           dependent: :destroy
   validates :name, presence: true
 end
