eaad19da4ce1ed2674116c283d9d0141fa5a33f3
keyasuda <keyasuda@users.noreply.github.com>
2022-01-16 19:20:36 +0900

    ETA/ETDの計算がおかしくなる問題を修正

--- trs/app/helpers/days_helper.rb
+++ trs/app/helpers/days_helper.rb
@@ -4,7 +4,7 @@ module DaysHelper
   def waypoint_eta(day, index)
     offset =
       day.routes[0, index].map { |r| r['summary']['time'].to_f }.compact.reduce(:+).to_i +
-      day.waypoints[0, index].map(&:stop_min).compact.reduce(:+).to_i * 60
+      day.ordered_waypoints[0, index].map(&:stop_min).compact.reduce(:+).to_i * 60
 
     offset.seconds.since(day.start_at)
   end
