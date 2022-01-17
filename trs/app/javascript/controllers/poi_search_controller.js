07fbecb3780d7d2edd26db93e02578293efd3371
keyasuda <keyasuda@users.noreply.github.com>
2022-01-17 21:17:25 +0900

    地名検索結果の選択処理をStimulusらしく

--- trs/app/javascript/controllers/poi_search_controller.js
+++ trs/app/javascript/controllers/poi_search_controller.js
@@ -4,21 +4,16 @@ export default class extends Controller {
   static targets = ['poi']
 
   connect() {
-    const onClick = (ev) => {
-      const target = ev.target
-      const lat = target.dataset.lat
-      const lon = target.dataset.lon
-
-      window.map.setView([lat, lon])
-      document.querySelector('#waypoint-name').value = target.innerText
-    }
-
-    Array.prototype.slice
-      .call(this.poiTargets)
-      .map((e) => e.addEventListener('click', onClick))
-
     if (this.poiTargets.length > 0) {
       this.poiTargets[0].click()
     }
+  }
+
+  select(e) {
+    const lat = e.target.dataset.lat
+    const lon = e.target.dataset.lon
+
+    window.map.setView([lat, lon])
+    document.querySelector('#waypoint-name').value = e.target.innerText
   }
 }
