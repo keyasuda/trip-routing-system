0557c875e440d344f9dee2975975f19fa709a662
keyasuda <keyasuda@users.noreply.github.com>
2026-09-02 23:23:38 +0900

    use config.load_defaults 8.0

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Trs
   # Main application configuration
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -27,8 +27,5 @@ module Trs
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
-
-    # Preserve full timezone in to_time for Rails 8.1 compatibility
-    config.active_support.to_time_preserves_timezone = :zone
   end
 end
